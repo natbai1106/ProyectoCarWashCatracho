ae3c9c19994a97dd24b8950702b8da3e81b0c6c6
dmhernanandez <dany1999hernan@gmail.com>
2021-07-26 21:47:26 -0600

    Se agrego el receptor de mensajes y las librarias de IOS

---
+++ PRMOVIL2CARWASH/PRMOVIL2CARWASH.Android/HashKey/HashKeyAndroid.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Auth.Api.Phone;
+using Android.Gms.Tasks;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PRMOVIL2CARWASH.Droid.HashKey;
+using PRMOVIL2CARWASH.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HashKeyAndroid))]
+namespace PRMOVIL2CARWASH.Droid.HashKey
+{
+    public class HashKeyAndroid : IHashService
+    {
+        [Obsolete]
+        public string GenerateHash()
+        {
+            AppHashKeyHelper hashKeyGenerator = new AppHashKeyHelper();
+            return hashKeyGenerator.GetAppHashKey(global::Android.App.Application.Context);
+
+        }
+
+        public void StartSMSRetriverReciber()
+        {
+
+            /*Se crea un instancia del recuperador de mensaje*/
+            SmsRetrieverClient _cliente = SmsRetriever.GetClient(global::Android.App.Application.Context);
+            /*
+             Se inicializa el Retriever para esparar por difución la llegada de un nuevo mensaje
+             */
+            _cliente.StartSmsRetriever();
+
+        }
+    }
+
+    internal class SuccessListener : Java.Lang.Object, IOnSuccessListener
+    {
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class FailureListener : Java.Lang.Throwable, IOnFailureListener
+    {
+
+        public void OnFailure(Java.Lang.Exception e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PRMOVIL2CARWASH/PRMOVIL2CARWASH/Utils/Constanst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRMOVIL2CARWASH.Utils
+{
+     public class Constanst
+    {
+        public const string UrlBase = "";
+
+    }
+}

7aae683ee6e8036b15c61e012e440916b9b1d8b1
natbai1106 <natbai1106@gmail.com>
2021-08-01 21:59:47 -0600

    Se agregaron las clases de modelos, motortype, typevehicle, brand, se modifico para poder llenar los picker desde los viewmodel

--- PRMOVIL2CARWASH/PRMOVIL2CARWASH/ViewModels/VehiclesViewModel.cs
+++ PRMOVIL2CARWASH/PRMOVIL2CARWASH/ViewModels/VehiclesViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using PRMOVIL2CARWASH.Utils;
-using System;
 using Xamarin.Forms;
+using System.Linq;
+using PRMOVIL2CARWASH.Models;
 
 namespace PRMOVIL2CARWASH.ViewModels
 {
@@ -15,26 +15,51 @@ namespace PRMOVIL2CARWASH.ViewModels
 
         Page Page;
 
-        string brand;
-        string modelo;
-        string type;
-        string motor;
+        List<Brand> brand;
+        List<Modelos> modelo;
+        List<TypeVehicle> type;
+        List<MotorType> motor;
+        int idmotor;
+        int idmarca;
+        int idmodelo;
+        int idtipovehiculo;
         int year;
         string plaque;
         string observation;
         byte[] foto;
         ImageSource photo = ImageSource.FromUri(new Uri("https://images.vexels.com/media/users/3/204554/isolated/lists/53193fd7db3d2618ab56635e69e64515-pequenas-hojas-de-frutos-rojos.png"));
 
-        public string Brand { get => brand; 
+        public int IdMotor
+        {
+            get => idmotor;
+            set => SetProperty(ref idmotor, value);
+        }
+        public int IdMarca
+        {
+            get => idmarca;
+            set => SetProperty(ref idmarca, value);
+        }
+        public int IdModelo
+        {
+            get => idmodelo;
+            set => SetProperty(ref idmodelo, value);
+        }
+        public int IdTypeVehicle
+        {
+            get => idtipovehiculo;
+            set => SetProperty(ref idtipovehiculo, value);
+        }
+
+        public List<Brand> Brand { get => brand; 
             set => brand = value; }
 
-        public string Modelo { get => modelo; 
+        public List<Modelos> Modelo { get => modelo; 
             set => modelo = value; }
 
-        public string Type { get => type; 
+        public List<TypeVehicle> TypeVehicle { get => type; 
             set => type = value; }
 
-        public string Motor { get => motor; 
+        public List<MotorType> MotorType { get => motor; 
             set => motor = value; }
 
         public int Year { get => year; 
@@ -61,20 +86,45 @@ namespace PRMOVIL2CARWASH.ViewModels
         public VehiclesViewModel(Page pag)
         {
             Page = pag;
+            MotorType motorType = new MotorType();
+            Brand listaBrand = new Brand();
+            Modelos modelos = new Modelos();
+            TypeVehicle typeVehicle = new TypeVehicle();
+
+            Brand = listaBrand.GetMarcas().OrderBy(c => c.NombreMarca).ToList();
+            TypeVehicle = typeVehicle.GetTipos().OrderBy(c => c.NombreTipoVehicle).ToList();
+            MotorType = motorType.GetMotor().OrderBy(c => c.NombreMotor).ToList();
+            Modelo = modelos.GetModelos().OrderBy(c => c.NombreModelo).ToList();
+
             SaveInformation = new Command(OnRequestSave);
             OpenGalleryCommand = new Command(OnOpenGallery);
             TakePhotoCommand = new Command(OnTakePhoto);
         }
-
+        
         private async void OnRequestSave(object obj)
         {
-            if (!string.IsNullOrEmpty(Brand))
+            var ListaBrand = new List<Brand>();
+            if (ListaBrand.Count == 0)
             {
-                await Page.DisplayAlert("La marca de su vehículo es: ", Brand, "Ok");
+                await Page.DisplayAlert("Mensaje", "Debe seleccionar una Marca para su auto", "Ok");
             }
-            else
+
+            var ListaModelos = new List<Modelos>();
+            if (ListaModelos.Count == 0)
             {
-                await Page.DisplayAlert("La marca del vehículo no puede ir vacío", Brand, "ok");
+                await Page.DisplayAlert("Mensaje", "Debe seleccionar un Modelo para su auto", "Ok");
+            }
+
+            var ListaType = new List<TypeVehicle>();
+            if (ListaType.Count == 0)
+            {
+                await Page.DisplayAlert("Mensaje", "Debe seleccionar un Tipo de auto", "Ok");
+            }
+
+            var ListaMotor = new List<MotorType>();
+            if (ListaMotor.Count == 0)
+            {
+                await Page.DisplayAlert("Mensaje", "Debe seleccionar el Tipo de combustible para el Motor de su auto", "Ok");
             }
         }
         private async void OnOpenGallery()
@@ -84,7 +134,6 @@ namespace PRMOVIL2CARWASH.ViewModels
             bool isSuccess = await media.TakePicture();
             if (isSuccess)
                 Photo = media.Image;
-
             IsBusy = false;
         }
         private async void OnTakePhoto()

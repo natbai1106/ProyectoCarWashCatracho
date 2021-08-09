c72a55ad8d9177742b5b435b8d01bbb5a2bad1b1
natbai1106 <natbai1106@gmail.com>
2021-08-08 19:29:07 -0600

    Hice este commit para poder hacer un pull de la rama de Sarah

---
+++ PRMOVIL2CARWASH/PRMOVIL2CARWASH/Converters/IntToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PRMOVIL2CARWASH.Converters
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value) > 0;      
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;      
+        }
+    }
+}
